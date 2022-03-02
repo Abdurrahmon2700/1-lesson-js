4440e5e017b27fe18ddf1c3de4a30749d8791a58
Abdurrahmon2700 <sabrin1998@inbox.ru>
2022-03-02 09:06:13 +0500

    co commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let a = +prompt("i: ");
+let b = +prompt("v: ");
+
+
+
+let s = (1/6 * Math.sqrt(a) + 1/3 * Math.sqrt(b)) ** 2;
+console.log(s);
---
+++ index2.js
@@ -0,0 +1,189 @@
+//  qaysi son katta bo'lsa ush sonni chiqarsin teng bo'lsa teng desin 
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let s =  a > b  ? "a katta": a==b ? "teng": "b katta"
+
+// console.log(s);
+
+
+
+
+// integer 2 
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let s = ( 1/6 * Math.sqrt(a) + 1/3 * Math.sqrt(b) )** 2;
+// console.log(s);
+
+
+//  integer 3
+
+
+// let son = +prompt("son kiriting: ");
+
+// let h = (son % 10) * 100 + parseInt ((son / 10) %10) * 10 + parseInt (son/100);
+
+// console.log(h);
+
+
+// integer 4
+
+// let a = +prompt("a: ");
+
+// let b = a % 10; 
+// let c = a % 100;
+// let d = c /10;
+// let g = a/100;
+
+// if (Math.round (d) < Math.round(g)) {
+//   console.log(Math.round(d));
+// }
+
+
+// // integer 5
+
+// let a = +prompt("a ");
+// let b = +prompt("b ");
+
+// let s = 9 * a ** 2 * b - 27 * a ** 2 * b ** 2 +15 * b ** 2;
+
+// console.log (Math.round(s));
+
+// integer 6
+
+// let r = +prompt("r ");
+// let n = +prompt("n ");
+// let a = +prompt("a ");
+// let b = +prompt("b ");
+
+// let g = (1+r/100)**n/Math.sqrt(a**2 + b**2);
+
+// console.log(g);
+
+// integer 7
+
+// let x1 = +prompt("x1 ");
+// let y1 = +prompt("y1 ");
+// let x2 = +prompt("x2 ");
+// let y2 = +prompt("y2 ");
+
+// let g = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+// console.log(g);
+
+//  integer 9 
+
+// let a = +prompt("a ");
+
+// let g = x=Math.sin(a);
+
+// console.log(g);
+
+
+//  integer 10 
+
+// let a = +prompt("a ");
+
+// let g = x=Math.cos(a);
+
+// console.log(g);
+
+
+//  integer 11
+
+// let a = +prompt("a ");
+// let b = +prompt("b ");
+
+// let g = a+b;
+// let s = a*b;
+
+// console.log(g,s);
+
+//  integer 12
+// let n = +prompt("n ");
+
+// let s = Math.pow(2.(n+3)**2);
+
+// console.log(s);
+
+
+
+
+// bool 1
+// let a = +prompt("a ");
+// let b = +prompt("b ");
+
+// let  s = Boolean(a>=b);
+
+// console.log(s);
+
+// bool 2
+// let a = +prompt("a ");
+
+// let  s = Boolean(a==7);
+
+// console.log(s);
+
+// bool 3
+
+// let a = +prompt("a ");
+
+// let  s = Boolean(a>0);
+
+// console.log(s);
+
+// bool 4
+
+// let a = +prompt("a ");
+
+// let  s = Boolean(a<0);
+
+// console.log(s);
+
+
+// bool 5
+
+// let a = +prompt("a ");
+
+// let  s = Boolean(a%2 !=0);
+
+// console.log(s);
+
+// bool 6
+
+// let a = +prompt("a ");
+
+// let  s = Boolean(a%2==0);
+
+// console.log(s);
+
+// bool 8
+
+// let a = +prompt ("a ");
+// let b = +prompt ("b ");
+
+// let g = (a>0 && b>0);
+
+// console.log(g);
+
+// bool 9
+
+// let a = +prompt ("a ");
+// let b = +prompt ("b ");
+
+// let g = (a<0 && b<0);
+
+// console.log(g);
+
+// bool 10
+
+// let a = +prompt ("a ");
+// let b = +prompt ("b ");
+
+// let g = (a%2==0 && b%2==0);
+
+// console.log(g);
+
+
